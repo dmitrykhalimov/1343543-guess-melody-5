2cf65e0ae0b9ef81c39b44381fbb05ef9dd32916
Bluesman <dmitry.khalimov@gmail.com>
2020-10-21 16:22:32 +0300

    Добавляет бизнес-логику и компонент mistake

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -18,6 +18,9 @@ const reducer = (state = initialState, action) => {
       return extend(state, {
         mistakes: state.mistakes + action.payload,
       });
+
+    case ActionType.RESET_GAME:
+      return extend({}, initialState);
   }
 
   return state;
